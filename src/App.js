28e6e527de4ee2bf96f4431dfd40a2afa82bef8f
microsoftjulius <julisema4@gmail.com>
2022-07-15 10:42:17 +0300

    cha

--- src/App.js
+++ src/App.js
@@ -1,65 +1,3 @@
-<<<<<<< HEAD
-import { isEmpty } from 'lodash';
-import React, { Suspense, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { useToasts } from 'react-toast-notifications';
-import { PageLoader } from './components/common';
-import { appActions } from './config/actions';
-import RoutePaths from './config/routes/RoutePaths';
-import Dashboard from './containers/Dashboard';
-import PageNotFound from './containers/Errors/PageNotFound';
-import usePrevious from './containers/Hooks/usePrevious';
-import './custom.scss';
-
-function App() {
-  const dispatch = useDispatch();
-  const { addToast, removeAllToasts } = useToasts();
-  const { serverSuccess, serverError } = useSelector((state) => state.server);
-  const { metadata } = useSelector((state) => state.app);
-  const prevState = usePrevious({ serverSuccess, serverError });
-
-  useEffect(() => {
-    if (isEmpty(metadata)) {
-      dispatch(appActions.getMetadata());
-    }
-  }, []); 
-
-  useEffect(() => {
-    if (!isEmpty(prevState)) {
-      if (!isEmpty(serverError) && serverError !== prevState.serverError) {
-        removeAllToasts();
-        addToast(serverError?.server?.message, {
-          autoDismiss: true,
-          appearance: 'error',
-          autoDismissTimeout: 6000,
-        });
-      }
-      if (
-        !isEmpty(serverSuccess) &&
-        serverSuccess !== prevState.serverSuccess
-      ) {
-        addToast(serverSuccess?.server?.message, {
-          autoDismiss: true,
-          appearance: 'success',
-        });
-      }
-    }
-  }, [serverError, serverSuccess]);
-
-  return (
-    <Suspense fallback={<PageLoader />}>
-      <Router>
-        <Switch>
-          <Route exact path={RoutePaths.dashboard.path} component={Dashboard} />
-          <Route path="*" component={PageNotFound} />
-        </Switch>
-      </Router>
-    </Suspense>
-  );
-}
-
-=======
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ScrollToTop from './helper/ScrollToTop';
@@ -122,5 +60,4 @@ function App() {
     )
 }
 
->>>>>>> d437d9939eaf731bc90228307a4e62ad7407a6ae
 export default App;
